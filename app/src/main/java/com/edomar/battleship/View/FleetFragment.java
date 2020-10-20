d79af65619fc5d17b953607172fea0436ec3ff04
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2020-10-20 18:33:57 +0200

    created fragment and tested flow throw menus

---
+++ app/src/main/java/com/edomar/battleship/View/FleetFragment.java
@@ -0,0 +1,49 @@
+package com.edomar.battleship.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edomar.battleship.R;
+
+public class FleetFragment extends Fragment {
+
+    public static final String TAG = FleetFragment.class.getSimpleName();
+
+    private HudActivity mActivity;
+
+    public FleetFragment() {
+        //Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        mActivity = (HudActivity) activity;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mActivity= null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fleet, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/com/edomar/battleship/View/SettingsFragment.java
@@ -0,0 +1,49 @@
+package com.edomar.battleship.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edomar.battleship.R;
+
+public class SettingsFragment extends Fragment {
+
+    public static final String TAG = SettingsFragment.class.getSimpleName();
+
+    private HudActivity mActivity;
+
+    public SettingsFragment() {
+        //Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        super.onAttach(activity);
+        mActivity = (HudActivity) activity;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mActivity= null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+}
